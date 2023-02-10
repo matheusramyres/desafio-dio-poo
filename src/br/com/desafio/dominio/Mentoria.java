2e153dc12784a97cd7774270c1a4682b7ebd8512
matheusramyres <matheus63360@gmail.com>
2023-02-10 16:21:16 -0300

    Mentoria e curso

---
+++ src/br/com/desafio/dominio/Mentoria.java
@@ -0,0 +1,46 @@
+package br.com.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo;
+    private String descricao;
+    private LocalDate data;
+
+    public Mentoria() {
+
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return this.data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " titulo='" + titulo + "'" +
+                ", descricao='" + descricao + "'" +
+                ", cargaHoraria='" + data + "'" +
+                "}";
+    }
+}
